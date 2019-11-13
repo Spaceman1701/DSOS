c339d3c3ece93526652fe7cd9d9ecd7bf7ae69a7
Ethan Hunter <fc.spaceman@gmail.com>
2019-11-13 15:42:42 -0700

    begin fleshing out ISA

--- compiler/compiler/src/main/scala/io/github/spaceman1701/oxidizer/compiler/bytecode/Bytecode.scala
+++ compiler/compiler/src/main/scala/io/github/spaceman1701/oxidizer/compiler/bytecode/Bytecode.scala
@@ -1,20 +1,34 @@
 package io.github.spaceman1701.oxidizer.compiler.bytecode
 
-sealed trait Bytecode
+sealed trait Instruction {
+  case class Store(loc: Long) extends Instruction
+  case class LoadConst(value: Long) extends Instruction
+  case class LoadVar(loc: Long) extends Instruction
 
-case object Pop
-case object LoadConst
-case object LoadVar
+  case object LoadArray extends Instruction //objref, index -> objref
 
-case object Add
-case object Sub
-case object Mul
-case object Div
-case object Mod
-case object Concat
-case object Pow
+  case object Add extends Instruction
+  case object Sub extends Instruction
+  case object Mul extends Instruction
+  case object Div extends Instruction
+  case object Mod extends Instruction
+  case object Concat extends Instruction
+  case object Pow extends Instruction
 
-case object CompGT
-case object CompGE
-case object CompLT
-case object CompEq
+  case object CompG extends Instruction
+  case object CompL extends Instruction
+  case object CompEq extends Instruction
+
+  case object GetMember extends Instruction//objref, name -> objref
+
+  case object Call extends Instruction//objref -> []
+
+  case class IfG(target: Byte) extends Instruction
+  case class IfL(target: Byte) extends Instruction
+  case class IfEq(target: Byte) extends Instruction
+  case class IfGWide(target: Long) extends Instruction
+  case class IfLWide(target: Long) extends Instruction
+  case class IfEqWide(target: Long) extends Instruction
+
+}
+
