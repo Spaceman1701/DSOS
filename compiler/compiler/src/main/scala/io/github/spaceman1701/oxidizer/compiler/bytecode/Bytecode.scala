668263f643dc806e093d84b90516d6502bec8346
Ethan Hunter <fc.spaceman@gmail.com>
2019-11-14 16:40:49 -0700

    work on bytecode

--- compiler/compiler/src/main/scala/io/github/spaceman1701/oxidizer/compiler/bytecode/Bytecode.scala
+++ compiler/compiler/src/main/scala/io/github/spaceman1701/oxidizer/compiler/bytecode/Bytecode.scala
@@ -15,20 +15,20 @@ sealed trait Instruction {
   case object Concat extends Instruction
   case object Pow extends Instruction
 
-  case object CompG extends Instruction
+  case object CompG extends Instruction //objref, objref -> value
   case object CompL extends Instruction
   case object CompEq extends Instruction
 
-  case object GetMember extends Instruction//objref, name -> objref
+  case object LoadMember extends Instruction//objref, name -> objref
 
   case object Call extends Instruction//objref -> []
 
-  case class IfG(target: Byte) extends Instruction
+  case class IfG(target: Byte) extends Instruction//objref, objref -> []
   case class IfL(target: Byte) extends Instruction
   case class IfEq(target: Byte) extends Instruction
-  case class IfGWide(target: Long) extends Instruction
-  case class IfLWide(target: Long) extends Instruction
-  case class IfEqWide(target: Long) extends Instruction
+  case class IfGWide(target: Int) extends Instruction //jumps can only happen inside a file
+  case class IfLWide(target: Int) extends Instruction
+  case class IfEqWide(target: Int) extends Instruction
 
 }
 
